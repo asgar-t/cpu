5c5d30f0e6a5390cb1f08b43ac9be1f3a04ffdd9
asgar-t <asgartalajawala@gmail.com>
2025-08-05 14:10:29 -0400

    ip testing

--- cpu.srcs/sources_1/new/divider.v
+++ cpu.srcs/sources_1/new/divider.v
@@ -1,26 +1,20 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/31/2025 11:24:41 AM
-// Design Name: 
-// Module Name: divider
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module divider(
-
+    input clk,
+    input [31:0] a,
+    input [31:0] b,
+    output reg [31:0] quot,
+    output reg [31:0] remainder
     );
+
+    // Internal wire to hold combined remainder+quotient from IP
+    wire [63:0] div_result;
+
+    
+    // Separate the combined 64-bit result into remainder and quotient
+    always @(posedge clk) begin
+        // Assign output ports from internal wire
+        quot <= div_result[31:0];
+        remainder <= div_result[63:32];
+    end
+
 endmodule
